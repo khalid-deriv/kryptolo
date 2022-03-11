ef4832e07686b4c0c1c3541891b44c7f60c4d433
Khalid Ibrahim <khalid@deriv.com>
2022-03-11 20:47:20 +0800

    contract indent

--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -1,21 +1,21 @@
 const main = async () => {
-  // We get the contract to deploy
-  const Transactions = await hre.ethers.getContractFactory("Transactions");
-  const transactions = await Transactions.deploy();
+    // We get the contract to deploy
+    const Transactions = await hre.ethers.getContractFactory("Transactions");
+    const transactions = await Transactions.deploy();
 
-  await transactions.deployed();
+    await transactions.deployed();
 
-  console.log("Transactions deployed to:", transactions.address);
+    console.log("Transactions deployed to:", transactions.address);
 };
 
 const runMain = async () => {
-  try {
-    await main();
-    process.exit(0);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
 };
 
 runMain();
